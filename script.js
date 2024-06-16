2ebd937dfa9d7ae1da891dbd8fbaaaae5b7f0faf
David Rajchenberg <davidhk28@gmail.com>
2024-06-16 19:31:53 +0300

    Worked on main program's code

--- script.js
+++ script.js
@@ -11,17 +11,35 @@ const sgPlay =  document.querySelector('#sg-play');
 const sgRegularBoard = document.querySelector('#sg-regular-board');
 const sgGameBoard = document.querySelector('#sg-game-board');
 
-let count = 20;
+let count = 10;
 let rows = [];
 let color = "grey";
 let flip = false;
 let char = "#";
+let charSize = ""
 let result = "";
+let shape = "pyramid";
 const space = " ";
 
 function makeRow(rowNum) {
-    
-    return space.repeat(count - rowNum) + char.repeat(rowNum) + space.repeat(count - rowNum);
+
+    let selectedShape;
+    switch (shape) {
+        case "pyramid":
+            selectedShape = space.repeat(count - rowNum) + char.repeat(rowNum) + space.repeat(count - rowNum);
+            break;
+        case "square":
+            selectedShape = char.repeat(count * 2);
+            break;
+        case "diamond":
+            selectedShape = function makeDiamond() {
+                let halfedCount = Math.floor(count / 2);
+                let diamond = space.repeat(count - rowNum) 
+                + char.repeat(rowNum) + space.repeat(count - rowNum);
+            }
+            break;
+    }
+    return selectedShape;
 }
 
 function fillShape() {
@@ -52,7 +70,7 @@ function printShape() {
 
 function printShapeForFeature() {
 
-    if (!sgRegularBoard == "" && !sgRegularBoard.innerText.includes("Click on 'Generate' or 'Play'"))
+    if (!sgRegularBoard.innerText == "" && !sgRegularBoard.innerText.includes("Click on 'Generate' or 'Play'"))
         sgRegularBoard.innerText = stackShape();
 }
 
@@ -60,7 +78,7 @@ function clear() {
     
     rows.length = 0;
     result = "";
-    sgRegularBoard.innerText = "";
+    sgRegularBoard.innerText = `Click on 'Generate' or 'Play' ${String.fromCodePoint(0x1F60E)}`;
 }
 
 function reset() {
@@ -70,7 +88,6 @@ function reset() {
 function play() {
 
 }
-
 
 // Click event listeners
 sgGenerate.onclick = printShape;
@@ -82,5 +99,30 @@ sgPlay.onclick = play;
 sgFlip.addEventListener('change', function(e) {
 
     flip = e.target.checked ? true : false;
+    printShapeForFeature();
+});
+
+sgChar.addEventListener('change', function(e) {
+
+    char = e.target.value;
+    printShapeForFeature();
+});
+
+sgCharSize.addEventListener('change', function (e) {
+    
+    charSize = e.target.value;
+    sgRegularBoard.style.fontSize = charSize + "px";    
+    printShapeForFeature();
+}); 
+
+sgNumRows.addEventListener('change', function(e) {
+
+    count = e.target.value;
+    printShapeForFeature();
+});
+
+sgShape.addEventListener('change', function(e) {
+
+    shape = e.target.value;
     printShapeForFeature();
 });
